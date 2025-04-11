2dffc5de01a960d3ea41e31d669621ec38989256
zozobkm <zmatallah42@gmail.com>
2025-04-11 15:35:41 +0200

    Update generate_graph.py

--- generate_graph.py
+++ generate_graph.py
@@ -2,56 +2,32 @@ import pygal
 import sqlite3
 from datetime import datetime
 
-def generate_cpu_graph():
+def generate_combined_graph():
     # Connexion à la base de données
     conn = sqlite3.connect('/home/uapv2305487/projet/monitoring.db')
     cursor = conn.cursor()
 
-    # Extraire les données de CPU de la table 'mesures'
-    cursor.execute("SELECT valeur, date FROM mesures WHERE sonde='cpu' ORDER BY date DESC LIMIT 60")
+    # Extraire les données de CPU et RAM de la table 'mesures'
+    cursor.execute("SELECT valeur, date, sonde FROM mesures WHERE sonde IN ('cpu', 'ram') ORDER BY date DESC LIMIT 60")
     data = cursor.fetchall()
 
-    # Données pour le graphique
-    cpu_values = [float(value) for value, date in data]
-    dates = [date for value, date in data]
+    # Organiser les données pour le graphique
+    cpu_values = [float(value) for value, date, sonde in data if sonde == 'cpu']
+    ram_values = [float(value) for value, date, sonde in data if sonde == 'ram']
+    dates = [date for value, date, sonde in data]
 
     # Créer le graphique
     line_chart = pygal.Line()
-    line_chart.title = 'Utilisation CPU sur les 60 dernières minutes'
+    line_chart.title = 'Utilisation CPU et RAM sur les 60 dernières minutes'
     line_chart.x_labels = dates
     line_chart.add('CPU (%)', cpu_values)
-
-    # Sauvegarder le graphique sous format SVG
-    line_chart.render_to_file('cpu_usage.svg')
-
-    # Fermer la connexion à la base de données
-    conn.close()
-
-def generate_ram_graph():
-    # Connexion à la base de données
-    conn = sqlite3.connect('/home/uapv2305487/projet/monitoring.db')
-    cursor = conn.cursor()
-
-    # Extraire les données de RAM de la table 'mesures'
-    cursor.execute("SELECT valeur, date FROM mesures WHERE sonde='ram' ORDER BY date DESC LIMIT 60")
-    data = cursor.fetchall()
-
-    # Données pour le graphique
-    ram_values = [float(value) for value, date in data]
-    dates = [date for value, date in data]
-
-    # Créer le graphique
-    line_chart = pygal.Line()
-    line_chart.title = 'Utilisation RAM sur les 60 dernières minutes'
-    line_chart.x_labels = dates
     line_chart.add('RAM (%)', ram_values)
 
     # Sauvegarder le graphique sous format SVG
-    line_chart.render_to_file('ram_usage.svg')
+    line_chart.render_to_file('graphe.svg')
 
     # Fermer la connexion à la base de données
     conn.close()
 
 if __name__ == "__main__":
-    generate_cpu_graph()
-    generate_ram_graph()
+    generate_combined_graph()
