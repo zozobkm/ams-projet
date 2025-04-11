aa4e33783b5bb52e956a96131ab1e7790120380f
zozobkm <zmatallah42@gmail.com>
2025-04-11 14:20:49 +0200

    Update detect_crise.py

--- detect_crise.py
+++ detect_crise.py
@@ -42,7 +42,7 @@ def detecter_crise():
             print(f"Dernière utilisation CPU: {cpu}% et RAM: {ram}%")
 
             # Détection d'une situation de crise (par exemple, si CPU > 0 ou RAM > 0)
-            seuil_cpu = 1
+            seuil_cpu = 0
             seuil_ram = 1
             if cpu > seuil_cpu or ram > seuil_ram:
                 print(f"CRISE DETECTEE ! CPU: {cpu}% | RAM: {ram}%")
