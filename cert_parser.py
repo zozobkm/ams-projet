49dee33c85253b1ef645e5b5a8cc337a01ac8151
zozobkm <zmatallah42@gmail.com>
2025-04-11 14:25:16 +0200

    Update cert_parser.py

--- cert_parser.py
+++ cert_parser.py
@@ -1,28 +1,30 @@
 import re
 import requests
-from db import init_alerts_table , insert_alert, delete_old_alerts
+from db import init_alerts_table, insert_alert, delete_old_alerts
 from datetime import datetime
 
 def get_lastcert_alert():
-	url = "https://www.cert.ssi.gouv.fr/"
-	reponse = requests.get(url)
-	
+    url = "https://www.cert.ssi.gouv.fr/"
+    reponse = requests.get(url)
 
-	match = re.search(r'< div class_="last_alert">(.*?)</div>',reponse.text, re.DOTALL)
+    # Rechercher la dernière alerte dans le contenu HTML
+    match = re.search(r'<div class="last_alert">(.*?)</div>', reponse.text, re.DOTALL)
 
-	if match:
-		alert_text = match.group(1).strip()
-		return alert_text
-
-	return "Aucune alerte trouvee"
-
+    if match:
+        alert_text = match.group(1).strip()
+        return alert_text
+    return "Aucune alerte trouvée"
 
 def store_alert():
-	init_alerts_table()
-	delete_old_alerts()
-	alert = get_lastcert_alert()
-	insert_alert(alert)
-	
+    # Initialisation de la table et suppression des anciennes alertes
+    init_alerts_table()
+    delete_old_alerts()
+
+    # Récupérer la dernière alerte
+    alert = get_lastcert_alert()
+
+    # Insertion de l'alerte dans la base de données
+    insert_alert(alert)
 
 if __name__ == "__main__":
-	store_alert()
+    store_alert()
