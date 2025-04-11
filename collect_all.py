0ca44a8989c4ca6202d82a55b9ddf31d0e401881
zozobkm <zmatallah42@gmail.com>
2025-04-11 14:25:36 +0200

    Update collect_all.py

--- collect_all.py
+++ collect_all.py
@@ -1,27 +1,30 @@
-from db import init_db , insert_mesure, clean_old_mesures
+from db import init_db, insert_mesure, clean_old_mesures
 import psutil
 import subprocess
+
+# Initialisation de la base de données
 init_db()
 
-######cpu=psutil.cpu_percent(interval=1)
-cpu = 97
-insert_mesure('cpu',str(cpu))
+# Récupérer la valeur du CPU
+cpu = psutil.cpu_percent(interval=1)  # Utilisation du CPU en pourcentage
+insert_mesure('cpu', str(cpu))
 
-
+# Si le CPU dépasse 95%, considérer cela comme une crise
 if cpu > 95:
-	print("crise :cpu>95%")
+    print("Crise : CPU > 95%")
 
+# Récupérer l'utilisation du disque
+disk = psutil.disk_usage('/').percent  # Utilisation du disque
+insert_mesure('disque', str(disk))
 
-##disk = p sutil.disk.usage('/').percent
+# Si l'utilisation du disque dépasse 95%, considérer cela comme une crise
+if disk > 95:
+    print("Crise : Disque > 95%")
 
-disk=98
-insert_mesure('disque',str(disk))
+# Récupérer le nombre d'utilisateurs connectés
+users = subprocess.getoutput("who | wc -l")  # Nombre d'utilisateurs connectés
+insert_mesure('utilisateurs', users)
 
-if disk >95:
-	print("crise: disque>95%")
-
-users=subprocess.getoutput("who |wc -l")
-insert_mesure('utilisateurs',users)
-
+# Nettoyer les anciennes mesures, conserver les 60 dernières minutes
 clean_old_mesures(60)
 
