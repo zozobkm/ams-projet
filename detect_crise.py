9ab6d4204ac779f771790f8d03b7f39a03292a0f
zozobkm <zmatallah42@gmail.com>
2025-04-11 15:03:29 +0200

    Update detect_crise.py

--- detect_crise.py
+++ detect_crise.py
@@ -1,25 +1,29 @@
-from db import init_db , insert_mesure, clean_old_mesures
-from db import init_alerts_table , insert_alert, delete_old_alerts
+from db import init_db, insert_mesure, clean_old_mesures
+from db import init_alerts_table, insert_alert, delete_old_alerts
 from datetime import datetime
 from envoyer_email import envoyer_email
 import psutil
+import sqlite3  # Assurez-vous d'importer sqlite3
+
 # Initialisation de la base de données
 init_db()
 
 def detecter_crise():
-  # Connexion à la base de données avec le bon chemin
-        conn = sqlite3.connect('/home/uapv2305487/projet/monitoring.db')
-        cursor = conn.cursor()
+    # Connexion à la base de données avec le bon chemin
+    conn = sqlite3.connect('/home/uapv2305487/projet/monitoring.db')
+    cursor = conn.cursor()
 
-        # Interroger la table "mesures" pour récupérer les dernières valeurs de CPU et RAM
-        cursor.execute("SELECT cpu, ram FROM mesures ORDER BY timestamp DESC LIMIT 1;")
-        result = cursor.fetchone()
+    # Interroger la table "mesures" pour récupérer les dernières valeurs de CPU et RAM
+    cursor.execute("SELECT cpu, ram FROM mesures ORDER BY timestamp DESC LIMIT 1;")
+    result = cursor.fetchone()
+
     if result:
-      cpu,ram = result
-      print(f"Dernière utilisation CPU: {cpu}% et RAM: {ram}%")
-      # Seuils de crise
-      seuil_cpu = 80
-      seuil_ram = 80
+        cpu, ram = result
+        print(f"Dernière utilisation CPU: {cpu}% et RAM: {ram}%")
+
+        # Seuils de crise
+        seuil_cpu = 80
+        seuil_ram = 80
 
         if cpu >= seuil_cpu or ram >= seuil_ram:
             print(f"CRISE DETECTEE ! CPU: {cpu}% | RAM: {ram}%")
